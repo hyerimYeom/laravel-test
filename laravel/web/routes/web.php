d951ae1e1664361bb84392f00109e1d646b2ca9d
Hyerim Yeom <hyerim.yeom@kakao.com>
2021-11-12 04:53:56 +0900

    #1 first commit

---
+++ laravel/web/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('add');
+});
+
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/{id}', [UserController::class, 'show']);
